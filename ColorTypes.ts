6bc08fde7aa43a928d4d51f4a2d1c8e15849269c
Александр Малышев <a.malyshev@bzklinika.ru>
2025-07-11 21:24:38 +0300

    main - added code exampes

---
+++ ColorTypes.ts
@@ -0,0 +1,50 @@
+export type ColorsType = {
+  main: string;
+  dark: string;
+  light: string;
+  extra: string;
+  background: string;
+  color: string;
+}
+
+export type ColorsBrightness = {
+  foreground: string;
+  customProp: string;
+};
+
+export type ColorsLowBrightness = {
+  white: string;
+};
+
+export type ColorsMediumBrightness = {
+  shadow: string;
+};
+
+export type ColorsHighBrightness = {
+  someProp: string;
+  anotherProp: string;
+  thirdCustomProp: string;
+};
+
+export type ColorsUltraBrightness = {
+  intensive: string;
+};
+
+export type ColorsDepth = {
+  background: string;
+  foreground: string;
+  color: string;
+};
+
+export type Colors8BitDepth = {
+  borderColor: string;
+};
+
+export type Colors16BitDepth = {
+  borderColor: string;
+  anotherColor: string;
+};
+
+export type Colors24BitDepth = {
+  extraColor: string;
+};
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # color-tones-scheme
 Functions with using of colors and tones scheme
+
+Это примеры кода функции для тестового задания. 
+
+Рабочую версию, интегрированную в реакт приложение можно посмотреть здесь: https://kadario.github.io/
+
+Примеры запуска можно посмотреть по ссылке: https://github.com/kadario/repo-list/blob/main/src/pages/ColorTonesPage.tsx
+
+Запустить локально - https://github.com/kadario/repo-list/tree/main
+
